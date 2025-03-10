c406b2b2e37f807b7df99a98d1b4de6bce10ffa2
0x1luxa <0x1luxa@gmail.com>
2025-03-10 13:09:54 +0400

    Add React logo to App component and clean up Vite config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import ReactLogo from '@/assets/react.svg'
+
 function App() {
   return (
     <form>
       <label className="block">
         <span className="...">Email</span>
+        <ReactLogo />
         <input
           type="email"
           className="peer flex size-13 flex-1 justify-around bg-black"
